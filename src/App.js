6389a6ea5f62503b3641aa478d0d2857ee393971
eshapatel2005 <esha.patel5605@gmail.com>
2023-10-11 18:16:54 +0530

    project completed

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
 
 
    let obj={
-    img1:'https://cutewallpaper.org/24/people-icon-png/group-together-teamwork-icon-people-icon-flat-png-free-transparent-png-clipart-images-download.png'
+    img1:'https://cutewallpaper.org/24/people-icon-png/group-together-teamwork-icon-people-icon-flat-png-free-transparent-png-clipart-images-download.png',
+    img2:'https://static.vecteezy.com/system/resources/previews/021/796/929/original/24-hours-servises-24-7-support-technical-support-customer-help-tech-support-customer-service-business-and-technology-concept-wireframe-hand-touching-digital-interface-illustration-vector.jpg'
    }
 
   return (
--- src/components/fronted.js
+++ src/components/fronted.js
@@ -4,11 +4,58 @@ import './fronted.css';
 export default function Navs(props){
     return(
         <>
-             <nav>
+            <nav className="container">
                 <div>
                     <img src={props.img1} style={{width:"50px"}}></img>
                 </div>
-             </nav>
+
+                <ul>
+                    <li>HOME</li>
+                    <li>ABOUT</li>
+                    <li>CONTACT</li>
+                </ul>
+            </nav>
+
+            <div className="contact_section">
+                <h1>CONTACT US</h1>
+                <p>LET'S CONNECT: WE'RE HERE TO HELP,AND WE'D LOVE TO HERE FROM YOU! WHETHER YOU HAVE A QUESTION,COMMENT,OR JUST WANT TO CHAT ,YOU CAN REACH OUT TO US THROUGH THE CONTACT FOEM OF THIS PAGE,OR BY PHONE,EMAIL,OR SOCIAL MEDIA. </p>
+            </div>
+
+            <div className="container1">
+                <div className="conatct_form">
+                    
+                    <button className="primary-btn">VIA SUPPORT CHAT</button>
+                    <button className="primary-btn1">VIA CALL</button>
+                 
+                    <button className="primary-btn2">VIA EMAIL FORM</button>
+                    <div>
+                        <form className="form1">
+                           <div className="form_control">
+                             <label>NAME :</label>
+                             <input type="text" name="NAME" />
+                           </div>
+
+                           <div className="form_control">
+                             <label>EMAIL :</label>
+                             <input type="email" name="EMAIL" />
+                           </div>
+
+                            <div className="form_control">
+                             <label className="input">TEXT :</label>
+                             <textarea className="textarea"  name="TEXT" />
+                           </div>
+
+                           <div>
+                            <button className="button1">SUBMIT</button>
+                           </div>
+                        </form>
+                    </div>
+                </div>
+                <div className="conatct_form1">
+
+                    <img src={props.img2} className='img2'></img>
+                </div>
+            </div>
         </>
     )
 }
